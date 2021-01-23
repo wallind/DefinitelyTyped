9c1871fd1f1ce165fe2d346f14f93135ada550a1
unknown <wallind@mail.gvsu.edu>
2021-01-23 15:47:46 -0500

    Initial push testing through all the definitions. got up to addUsersToRoles covered

--- types/meteor-roles/meteor-roles-tests.ts
+++ types/meteor-roles/meteor-roles-tests.ts
@@ -1,149 +1,94 @@
 import { Roles } from 'meteor/alanning:roles'
-import * as _ from 'underscore';
-
-/**
- * All code below was copied from the examples at https://github.com/alanning/meteor-roles/.
- * When necessary, code was added to make the examples work (e.g. declaring a variable
- * that was assumed to have been declared earlier)
- */
-
-var joesUserId = '1234';
-Roles.addUsersToRoles(joesUserId, ['manage-team','schedule-game'], 'manchester-united.com')
-Roles.addUsersToRoles(joesUserId, ['player','goalie'], 'real-madrid.com')
-
-Roles.userIsInRole(joesUserId, 'manage-team', 'manchester-united.com')  // => true
-Roles.userIsInRole(joesUserId, 'manage-team', 'real-madrid.com')  // => false
-
-Roles.addUsersToRoles(joesUserId, 'super-admin', Roles.GLOBAL_GROUP)
-
-var bobsUserId = '1234';
-Roles.addUsersToRoles(bobsUserId, ['manage-team','schedule-game'])
-// internal representation - no groups
-// user.roles = ['manage-team','schedule-game']
-
-Roles.addUsersToRoles(joesUserId, ['manage-team','schedule-game'], 'manchester-united.com')
-Roles.addUsersToRoles(joesUserId, ['player','goalie'], 'real-madrid.com')
-// internal representation - groups
-// NOTE: MongoDB uses periods to represent hierarchy so periods in group names
-//   are converted to underscores.
-//
-// user.roles = {
-//   'manchester-united_com': ['manage-team','schedule-game'],
-//   'real-madrid_com': ['player','goalie']
-// }
-
-Meteor.roles.find({});
 
 
+// $ExpectType string
+Roles.GLOBAL_GROUP
 
-var users = [
-    {name:"Normal User",email:"normal@example.com",roles:[]},
-    {name:"View-Secrets User",email:"view@example.com",roles:['view-secrets']},
-    {name:"Manage-Users User",email:"manage@example.com",roles:['manage-users']},
-    {name:"Admin User",email:"admin@example.com",roles:['admin']}
-];
-
-_.each(users, function (user) {
-    var id : string;
-
-    id = Accounts.createUser({
-        email: user.email,
-        password: "apple1",
-        profile: { name: user.name }
-    });
-
-    if (user.roles.length > 0) {
-        // Need _id of existing user record so this call must come
-        // after `Accounts.createUser` or `Accounts.onCreate`
-        Roles.addUsersToRoles(id, user.roles, 'default-group');
-    }
-
-});
+// $ExpectType Subscription
+Roles.subscription
 
 
+/*** createRole tests ***/
+// createRole without options
+// $ExpectType string
+Roles.createRole('createRole_singleParam')
 
-// server/publish.js
+// createRole with options
+// $ExpectType string
+Roles.createRole('createRole_doubleParam', { unlessExists: true })
 
-// Give authorized users access to sensitive data by group
-Meteor.publish('secrets', function (group : string) {
-    if (Roles.userIsInRole(this.userId, ['view-secrets','admin'], group)) {
-
-//        return Meteor.secrets.find({group: group});
-
-    } else {
-
-        // user not authorized. do not publish secrets
-        this.stop();
-        return;
-
-    }
-});
+/*** deleteRole tests ***/
+// $ExpectType void
+Roles.deleteRole('deleteRole')
 
 
-Accounts.validateNewUser(function (user : Meteor.User) {
-    var loggedInUser = Meteor.user();
-
-    if (Roles.userIsInRole(loggedInUser, ['admin','manage-users'])) {
-        // NOTE: This example assumes the user is not using groups.
-        return true;
-    }
-
-    throw new Meteor.Error('403', "Not authorized to create new users");
-});
+/*** renameRole tests ***/
+Roles.createRole('renameRole_originalName')
+// $ExpectType void
+Roles.renameRole('renameRole_originalName', 'renameRole_originalName_modified')
 
 
-// server/userMethods.js
+/*** addRolesToParent tests ***/
+Roles.createRole('parentRole_addRolesToParent')
+Roles.createRole('childRole_addRolesToParent')
 
-Meteor.methods({
-    /**
-     * delete a user from a specific group
-     *
-     * @method deleteUser
-     * @param {String} targetUserId _id of user to delete
-     * @param {String} group Company to update permissions for
-     */
-    deleteUser: function (targetUserId : string, group : string) {
-        var loggedInUser = Meteor.user()
+// addRolesToParent first param string[]
+// $ExpectType void
+Roles.addRolesToParent(['childRole_addRolesToParent'], 'parentRole_addRolesToParent')
 
-        if (!loggedInUser ||
-            !Roles.userIsInRole(loggedInUser,
-                ['manage-users', 'support-staff'], group)) {
-            throw new Meteor.Error('403', "Access denied")
-        }
-
-        // remove permissions for target group
-        Roles.setUserRoles(targetUserId, [], group)
-
-        // do other actions required when a user is removed...
-    }
-})
+// addRolesToParent first param string
+// $ExpectType void
+Roles.addRolesToParent('childRole_addRolesToParent', 'parentRole_addRolesToParent')
 
 
+/*** removeRolesFromParent tests ***/
+Roles.createRole('parentRole_removeRolesFromParent')
+Roles.createRole('childRole1_removeRolesFromParent')
+Roles.createRole('childRole2_removeRolesFromParent')
+Roles.addRolesToParent(
+    ['childRole1_removeRolesFromParent', 'childRole2_removeRolesFromParent'], 'parentRole_removeRolesFromParent')
 
-// server/userMethods.js
+// removeRolesFromParent first param string[]
+// $ExpectType void
+Roles.removeRolesFromParent(['childRole1_removeRolesFromParent'], 'parentRole_removeRolesFromParent')
 
-Meteor.methods({
-    /**
-     * update a user's permissions
-     *
-     * @param {Object} targetUserId Id of user to update
-     * @param {Array} roles User's new permissions
-     * @param {String} group Company to update permissions for
-     */
-    updateRoles: function (targetUserId : string, roles : string[], group : string) {
-        var loggedInUser = Meteor.user()
-
-        if (!loggedInUser ||
-            !Roles.userIsInRole(loggedInUser,
-                ['manage-users', 'support-staff'], group)) {
-            throw new Meteor.Error('403', "Access denied")
-        }
-
-        Roles.setUserRoles(targetUserId, roles, group)
-    }
-})
+// removeRolesFromParent first param string
+// $ExpectType void
+Roles.removeRolesFromParent('childRole2_removeRolesFromParent', 'parentRole_removeRolesFromParent')
 
 
+// Shared resource user for testing
+const userId: string = Accounts.createUser({ username: 'user1', 'password': 'pass' })
+const user: Meteor.User = Meteor.users.findOne({ _id: userId })
 
 
+/*** addUsersToRoles tests ***/
+Roles.createRole('addUsersToRoles')
 
+// addUsersToRoles first param string; also tests second param string;
+// $ExpectType void
+Roles.addUsersToRoles(userId, 'addUsersToRoles')
+
+// addUsersToRoles first param string[]
+// $ExpectType void
+Roles.addUsersToRoles([userId], 'addUsersToRoles')
+
+// addUsersToRoles first param Object
+// $ExpectType void
+Roles.addUsersToRoles(user, 'addUsersToRoles')
+
+// addUsersToRoles first param Object[]
+// $ExpectType void
+Roles.addUsersToRoles([user], 'addUsersToRoles')
+
+// addUsersToRoles second param string[]
+// $ExpectType void
+Roles.addUsersToRoles(userId, ['addUsersToRoles'])
+
+// addUsersToRoles third param string
+// $ExpectType void
+Roles.addUsersToRoles(userId, 'addUsersToRoles', 'scope_addUsersToRoles')
+
+// addUsersToRoles third param Object
+// $ExpectType void
+Roles.addUsersToRoles(userId, 'role_does_not_exist', { ifExists: true })
